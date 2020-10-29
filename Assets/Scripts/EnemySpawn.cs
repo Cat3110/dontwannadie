10727ec92f2f214dac29dc022baf3cba4c48b4ae
Egor Vasilyev <egor@vasilyev.sexy>
2020-10-29 20:06:54 +0300

    Enemy spawn was added

---
+++ Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public enum EnemyType
+    {
+        spider,
+        caster
+    }
+
+    public EnemyType enemyType;
+
+    public GameObject enemy;
+
+    public GameObject[] waypoints;
+}
---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour
+{
+    private GameObject[] spawnPoints;
+
+    private void Awake()
+    {
+        spawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoint");
+
+        foreach(GameObject spawnPoint in spawnPoints)
+        {
+            GameObject enemy = Instantiate(spawnPoint.GetComponent<EnemySpawn>().enemy, spawnPoint.transform.position, spawnPoint.transform.rotation);
+
+            switch (spawnPoint.GetComponent<EnemySpawn>().enemyType)
+            {
+                case EnemySpawn.EnemyType.spider:
+                    break;
+                case EnemySpawn.EnemyType.caster:
+                    enemy.transform.Rotate(0, 90, 0);
+                    break;
+                default:
+                    break;
+            }
+
+            spawnPoint.SetActive(false);
+        }
+    } 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
